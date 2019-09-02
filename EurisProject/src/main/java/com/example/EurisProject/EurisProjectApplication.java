30158422446c562def67c11c3c9c5b80c54f000c
Emanuele Marussi <emanuele_marussi@hotmail.it>
2019-09-02 16:31:05 +0200

    Add Project Euris Project

---
+++ EurisProject/src/main/java/com/example/EurisProject/EurisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EurisProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurisProjectApplication.class, args);
+	}
+
+}
