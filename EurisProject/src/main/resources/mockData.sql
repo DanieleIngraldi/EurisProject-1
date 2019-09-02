b0055773324b974b0b13bf48861b81f6ac01e4d4
Emanuele Marussi <emanuele_marussi@hotmail.it>
2019-09-02 17:10:11 +0200

    Added Mock Data

---
+++ EurisProject/src/main/resources/mockData.sql
@@ -0,0 +1,16 @@
+INSERT INTO Module
+(id, Name, Surname, BirthDate, CreationTimestamp, Age, TYPE)
+VALUES
+(1, 'Jason', 'Statham', 26-Jul-1967, 2019-09-02 16:57:47.361, 52 , 'C'),
+(2, 'Sylvester', 'Stallone', 6-Jul-1946, 2019-09-02 16:57:47.39, 73, 'O'),
+(3, 'Jet', 'Li', 26-Apr-1963, 2019-09-02 16:57:47.394, 56, 'S'),
+(4, 'Antonio', 'Banderas', 10-Aug-1960, 2019-09-02 17:00:08.84, 59, 'C'),
+(5, 'Wesley', 'Snipes', 31-Jul-1962, 2019-09-02 17:10:08.37, 57, 'O'),
+(6, 'Mel', 'Gibson', 3-Jan-1956, 2019-09-02 17:15:20.00, 63, 'S'),
+(7, 'Dolph', 'Lundgren', 3-Nov-1957, 2019-09-02 17:18:32.02, 62, 'C'),
+(8, 'Harrison', 'Ford', 13-Jul-1942, 2019-09-02 17:22:17.16, 77, 'O'),
+(9, 'Arnold', 'Schwarzenegger', 30-Jul-1947, 2019-09-02 17:24:26.99, 72, 'S'),
+(10, 'Terry', 'Crews', 30-Jul-1968, 2019-09-02 17:32:44.54, 72, 'C'),
+(11, 'Bruce', 'Willis', 19-Mar-1955, 2019-09-02 17:41:22.22, 64, 'O'),
+(12, 'Chuck', 'Norris', 10-Mar-1940, 2019-09-02 17:45:37.32, 79, 'S'),
+(13, 'Jean-Claude', 'Van Damme', 18-Oct-1960, 2019-09-02 17:55:17.41, 59, 'C');
