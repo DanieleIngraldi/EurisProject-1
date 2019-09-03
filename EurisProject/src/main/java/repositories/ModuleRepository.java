24c8710ae8f05e6cfd5b34f5ea28b0e6e8a4e4a3
Emanuele Marussi <emanuele_marussi@hotmail.it>
2019-09-03 09:28:21 +0200

    Added application properties and ModuleRepository methods

--- EurisProject/src/main/java/repositories/ModuleRepository.java
+++ EurisProject/src/main/java/repositories/ModuleRepository.java
@@ -1,10 +1,18 @@
-package repositories;
+package Repositories;
 
+import model.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 public interface ModuleRepository extends JpaRepository<Module, Long> {
+    @Repository
+    public interface PersonsRepository extends JpaRepository<Module, Long> {
+        List<Module> findByName(String name);
 
-    
+        List<Module> findBySurname(String surname);
 
+        List<Module> findByAge(int age);
+    }
 }
